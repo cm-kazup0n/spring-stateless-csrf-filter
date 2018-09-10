8968a1cca9435c22e70bbfab8776e8bed545bd73
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2018-09-10 15:02:32 +0900

    カバレッジが低かったcoreのテストを追加

--- stateless-csrf-filter-core/src/test/java/jp/classmethod/spring_stateless_csrf_filter/session/CookieSessionTest.java
+++ stateless-csrf-filter-core/src/test/java/jp/classmethod/spring_stateless_csrf_filter/session/CookieSessionTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 public class CookieSessionTest {
 
+    private final TokenSigner signer = new TokenSigner("SECRET");
 
     @Test
     public void testPutAndGet() {
@@ -17,7 +18,6 @@ public class CookieSessionTest {
 
     @Test
     public void testSerDe() {
-        final TokenSigner signer = new TokenSigner("SECRET");
         final CookieSession session =
                 (CookieSession) CookieSession.create()
                         .put("key-1", "value")
@@ -31,6 +31,14 @@ public class CookieSessionTest {
         assertEquals(session.get("key-1").get(), restored.get("key-1").get());
         assertEquals(session.get("key-2").get(), restored.get("key-1").get());
         assertEquals(session.get("key-3").get(), restored.get("key-1").get());
+    }
+
+    @Test
+    public void 空のセッションをデシリアライズする(){
+        final CookieSession session = (CookieSession) CookieSession.create();
+        final String serialized = CookieSession.SerDe.serialize(signer, session);
+        final CookieSession deserialized = CookieSession.SerDe.deserialize(signer, serialized);
+        assertTrue(deserialized.getKeys().isEmpty());
     }
 
 
